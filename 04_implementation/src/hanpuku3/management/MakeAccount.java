9c15457970766d99b791256a48d2307e5149c463
Machida Takumi <t-machida@cresco.co.jp>
2020-06-12 11:25:40 +0900

    a

--- 04_implementation/src/hanpuku3/management/MakeAccount.java
+++ 04_implementation/src/hanpuku3/management/MakeAccount.java
@@ -1,14 +1,18 @@
 package management;
 
+import java.util.Map;
+
 import domain.Member;
 import domain.MemberList;
-import userInterface.Interface;
 
-public class MakeAccount {
+public class MakeAccount extends Command{
 	
 	//アカウント作成
-	public Member newAccount(Interface uiinterface, MemberList ml) {
-		int approval=0;
+	
+	@Override
+	public Object execute(Map<Integer,Object> map) {
+		MemberList ml = (MemberList)map.get(1);
+		Boolean approval=null;
 		Member member=null;
 		//情報の入力
 		while(true) {
@@ -32,18 +36,18 @@ public class MakeAccount {
 			uiinterface.output("----------------------------");
 			uiinterface.output("上記の内容でアカウント作成を行いますか？");
 			uiinterface.output("承認なら「1」,承認しないなら「2」を入力してください");
-			approval=uiinterface.inputInt();
-			if(approval==1) {//承認
+			approval=approval();
+			if(approval) {//承認
 				//メンバーリストに追加
 				member=ml.setMember(id,pass);
 				uiinterface.output("登録完了！続いてログインを行います。");
 				break;
-			}else if (approval == 2){//非承認
+			}else{//非承認
 				break;
-			}else {
-				uiinterface.output("「1」か「2」を入力してください");
 			}
 		}
 		return member;
 	}
 }
+
+	
