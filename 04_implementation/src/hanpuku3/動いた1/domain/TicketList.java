dad72c3c930fc795d628f4642592263b36aaed56
Machida Takumi <t-machida@cresco.co.jp>
2020-06-12 12:15:25 +0900

    a

---
+++ 04_implementation/src/hanpuku3/動いた1/domain/TicketList.java
@@ -0,0 +1,77 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class TicketList {
+	
+	ArrayList<Ticket> ticketList =new ArrayList<Ticket>();
+	
+	public TicketList() {
+	}
+	
+	//チケット名：チケット番号を表示　（チケット選択の時）
+	public String showTicketNumberName() {
+		String data = "チケット名:チケット番号=";
+		
+		for(Ticket t:ticketList) {
+			data+=t.getTicketName();
+			data+=":";
+			data+=t.getTicketNumber();
+			data+="=";
+		}
+		
+		return data;
+	}
+	
+	//リストに含まれるチケットオブジェクトを取得
+	public Ticket getTicket(int num) {
+		
+		for(Ticket t:ticketList ) {			
+			if(num == t.getTicketNumber()) {
+				return t;								//メンバーオブジェクト返す
+			}
+		}
+		return null;
+	}
+	
+	//全てのチケット情報を表示
+	public String showAllTicketData() {
+		String data="";
+		for(Ticket t:ticketList) {
+			data+=t.showTicketData();
+			data+="\n";
+			data+="---------"; 
+			data+="\n";
+		}
+		return data;
+	}
+	
+	//ファイルから取得したチケットリストをシステムのチケットリストに追加
+	public void douwnlodeTicketData(ArrayList<Ticket> fileTicketList) {
+		for(int num=0;num < fileTicketList.size();num++) {
+			Ticket ticket = fileTicketList.get(num);
+			ticketList.add(ticket);
+		}
+	}
+	
+	//システムのチケットリストをファイルのチケットリストに追加
+	public ArrayList<Ticket> updateTicketData (ArrayList<Ticket> fileTicketList) {
+		for(int num=0;num < ticketList.size();num++) {
+			Ticket newTicket = ticketList.get(num);
+			fileTicketList.add(newTicket);
+		}
+		return fileTicketList;
+	}
+	
+	//test
+	public void addTicket(Ticket ticket) {
+		ticketList.add(ticket);
+	}
+	
+	
+	public Ticket getTicketobj(int index){
+		return ticketList.get(index);
+	}
+	
+}
+
